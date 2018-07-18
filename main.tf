3a8e4ace71f18488f7e296d0fc0c27f9fdb00bcc
Soma <Vineeth.Soma@coxautoinc.com>
2018-07-18 10:35:07 -0400

    added loadbalancer

--- main.tf
+++ main.tf
@@ -20,3 +20,22 @@ resource "kubernetes_pod" "nginx" {
     }
   }
 }
+
+resource "kubernetes_service" "nginx" {
+  metadata {
+    name = "nginx-example"
+  }
+
+  spec {
+    selector {
+      App = "${kubernetes_pod.nginx.metadata.0.labels.App}"
+    }
+
+    port {
+      port        = 80
+      target_port = 80
+    }
+
+    type = "LoadBalancer"
+  }
+}
