75e18e1e08c9d548c77115c2fdcef8ff8846e138
Soma <Vineeth.Soma@coxautoinc.com>
2018-07-18 10:24:47 -0400

    inital commit

---
+++ main.tf
@@ -0,0 +1,22 @@
+provider "kubernetes" {}
+
+resource "kubernetes_pod" "nginx" {
+  metadata {
+    name = "nginx-example"
+
+    labels {
+      App = "nginx"
+    }
+  }
+
+  spec {
+    container {
+      image = "nginx:1.7.8"
+      name  = "example"
+
+      port {
+        container_port = 80
+      }
+    }
+  }
+}
